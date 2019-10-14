481991d24e70e65a0af9a3e5b723a4e2f007725d
zh-star <53932935+zh-star@users.noreply.github.com>
2019-10-14 23:11:10 +0800

    Update text.c

--- 20190813/20190813/text.c
+++ 20190813/20190813/text.c
@@ -3,8 +3,7 @@
 /*c语言编码，实现函数long htonl(long a)，将大端转换为小端
 题目意思是：将大端数据0x12345678 转换为：0x78563412
 */
-int main()
-{
+int main() {
 	//char ch = 'w';
 	//char *pc = &ch;
 	//*pc = 'w';
@@ -15,18 +14,15 @@ int main()
 	return 0;
 }
 #if 0
- long htonl(long a)
-{
+ long htonl(long a) {
 	 int i = 0;
 	 int b = 0x00000000;
-	 for (i = 0; i <32; i +=8)
-	 {
+	 for (i = 0; i <32; i +=8) {
 		 b = (b<<8) | ((a >> i) & 0xff);//1&任何数就等于任何数     0|任何数等于任何数     0^任何数等于任何数
 	 }
 	 return b;
 }
-int main()
-{
+int main() {
 	//char a[] = "0x12345678";
 	//char *p = &a;
 	printf("%x\n", htonl(0x12345678));
