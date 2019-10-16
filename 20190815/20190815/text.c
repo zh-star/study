66f96864d6b1cac63a8e19e82a9cfd3df679ed82
zh-star <53932935+zh-star@users.noreply.github.com>
2019-10-16 23:13:47 +0800

    Update text.c

--- 20190815/20190815/text.c
+++ 20190815/20190815/text.c
@@ -1,24 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int main()
-{
+int main() {
 	int arr[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 	int i = 0;
 	int j = 0;
-	printf("ת��ǰ��\n");
-	for (i = 0; i <4; i++)
-	{
-		for (j = 0; j<4; j++)
-		{
+	printf("×ªÖÃÇ°£º\n");
+	for (i = 0; i <4; i++) {
+		for (j = 0; j<4; j++) {
 			printf("%2d ", arr[i][j]);
 		}
 		printf("\n");
 	}
-	printf("ת�ú� \n");
-	for (i = 0; i <4; i++)
-	{
-		for (j = 0; j<4; j++)
-		{
+	printf("×ªÖÃºó£º \n");
+	for (i = 0; i <4; i++) {
+		for (j = 0; j<4; j++) {
 			printf("%2d ", arr[j][i]);
 		}
 		printf("\n");
